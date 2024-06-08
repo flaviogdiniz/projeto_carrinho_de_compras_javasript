2dbf2d4a3297f5305ce83a3e52922a89755c32a6
flaviogdiniz <flaviogdiniz@gmail.com>
2024-06-08 05:31:12 -0300

    Removendo Item

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ function adicionarItem(item){
     quantidade.innerHTML = qtd[item];
     total.innerHTML = valorTotal[item].toFixed(2);
 }
+
+function removerItem(item){
+    if(qtd[item] > 0){
+        qtd[item] -= 1;
+        var quantidade = document.getElementById("quantidade" + item);
+        var total = document.getElementById("total" + item);
+        quantidade.innerHTML = qtd[item];
+        valorTotal[item] = Number.parseFloat(valorProduto[item] * qtd[item]);
+        quantidade.innerHTML = qtd[item];
+        total.innerHTML = valorTotal[item].toFixed(2);
+    }
+}
